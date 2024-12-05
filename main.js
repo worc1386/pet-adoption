33a03b7b0e9b3d508702f794c8e2c06a39bd0b38
worc1386 <worc1386@gmail.com>
2024-12-05 13:02:48 +0000

    fchange pets jsaon linkt

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ async function start() {
 start()
 
 async function petsArea() {
-  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsPromise = await fetch("https://verdant-travesseiro-74cad5.netlify.app/.netlify/functions/pets")
   const petsData = await petsPromise.json()
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true)
